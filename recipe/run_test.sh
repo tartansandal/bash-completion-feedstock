9cc1f9d66ee44e2ceebb7c7e53325010f922fb49
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2025-08-28 11:34:58 +1000

    Use more well-defined conditional expression in test script

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,7 +1,7 @@
 # Check for recent enough version of bash.
 # This is the same check used in the integration script
-if [ ${BASH_VERSINFO[0]} -gt 4 ] || \
-    [ ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 ]; then
+if [ "${BASH_VERSINFO[0]}" -gt 4 ] ||
+    [ "${BASH_VERSINFO[0]}" -eq 4 ] && [ "${BASH_VERSINFO[1]}" -ge 1 ]; then
 
     # Ensure the library loads without error at least
     bash -e bash_completion
